6403f7f115abc0cca5664e900eee127c375cf811
Vegetable-SYC <109536807+Vegetable-SYC@users.noreply.github.com>
2026-06-13 14:30:46 +0800

    update

--- demos/music/lv_demo_music_list.c
+++ demos/music/lv_demo_music_list.c
@@ -56,8 +56,8 @@ lv_obj_t * _lv_demo_music_list_create(lv_obj_t * parent)
     font_small = &lv_font_montserrat_16;
     font_medium = &lv_font_montserrat_22;
 #else
-    font_small = &lv_font_montserrat_14;
-    font_medium = &lv_font_montserrat_14;
+    font_small = &lv_font_montserrat_12;
+    font_medium = &lv_font_montserrat_16;
 #endif
 
     lv_style_init(&style_scrollbar);
@@ -128,6 +128,18 @@ lv_obj_t * _lv_demo_music_list_create(lv_obj_t * parent)
     _lv_demo_music_list_btn_check(0, true);
 
     return list;
+}
+
+void _lv_demo_music_list_close(void)
+{
+    lv_style_reset(&style_scrollbar);
+    lv_style_reset(&style_btn);
+    lv_style_reset(&style_btn_pr);
+    lv_style_reset(&style_btn_chk);
+    lv_style_reset(&style_btn_dis);
+    lv_style_reset(&style_title);
+    lv_style_reset(&style_artist);
+    lv_style_reset(&style_time);
 }
 
 void _lv_demo_music_list_btn_check(uint32_t track_id, bool state)
@@ -205,7 +217,6 @@ static lv_obj_t * add_list_btn(lv_obj_t * parent, uint32_t track_id)
     return btn;
 }
 
-
 static void btn_click_event_cb(lv_event_t * e)
 {
     lv_obj_t * btn = lv_event_get_target(e);
@@ -215,4 +226,3 @@ static void btn_click_event_cb(lv_event_t * e)
     _lv_demo_music_play(idx);
 }
 #endif /*LV_USE_DEMO_MUSIC*/
-
--- src/draw/sw/lv_draw_sw_gradient.c
+++ src/draw/sw/lv_draw_sw_gradient.c
@@ -21,6 +21,7 @@
     #define GRAD_CONV(t, x) t = x
 #endif
 
+#undef ALIGN
 #if defined(LV_ARCH_64)
     #define ALIGN(X)    (((X) + 7) & ~7)
 #else
@@ -45,7 +46,6 @@ static lv_res_t kill_oldest_item(lv_grad_t * c, void * ctx);
 static lv_res_t find_item(lv_grad_t * c, void * ctx);
 static void free_item(lv_grad_t * c);
 static  uint32_t compute_key(const lv_grad_dsc_t * g, lv_coord_t w, lv_coord_t h);
-
 
 /**********************
  *   STATIC VARIABLE
@@ -86,8 +86,6 @@ static lv_grad_t * next_in_cache(lv_grad_t * item)
 
     if(item == NULL)
         return (lv_grad_t *)LV_GC_ROOT(_lv_grad_cache_mem);
-    if(item == NULL)
-        return NULL;
 
     size_t s = get_cache_item_size(item);
     /*Compute the size for this cache item*/
@@ -229,7 +227,6 @@ static lv_grad_t * allocate_item(const lv_grad_dsc_t * g, lv_coord_t w, lv_coord
     return item;
 }
 
-
 /**********************
  *     FUNCTIONS
  **********************/
@@ -294,7 +291,7 @@ lv_grad_t * lv_gradient_get(const lv_grad_dsc_t * g, lv_coord_t w, lv_coord_t h)
     return item;
 }
 
-LV_ATTRIBUTE_FAST_MEM lv_grad_color_t lv_gradient_calculate(const lv_grad_dsc_t * dsc, lv_coord_t range,
+lv_grad_color_t LV_ATTRIBUTE_FAST_MEM lv_gradient_calculate(const lv_grad_dsc_t * dsc, lv_coord_t range,
                                                             lv_coord_t frac)
 {
     lv_grad_color_t tmp;
@@ -325,6 +322,8 @@ LV_ATTRIBUTE_FAST_MEM lv_grad_color_t lv_gradient_calculate(const lv_grad_dsc_t 
             break;
         }
     }
+
+    LV_ASSERT(d != 0);
 
     /*Then interpolate*/
     frac -= min;
